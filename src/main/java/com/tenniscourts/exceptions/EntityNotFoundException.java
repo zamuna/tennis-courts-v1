be8d50fc8d90b7d561f3077ed092ff5d889b0807
Jamuna Ghale <zamuna16@gmail.com>
2021-08-02 15:41:23 -0500

    merge complete

--- src/main/java/com/tenniscourts/exceptions/EntityNotFoundException.java
+++ src/main/java/com/tenniscourts/exceptions/EntityNotFoundException.java
@@ -1,18 +1,9 @@
 package com.tenniscourts.exceptions;
 
-<<<<<<< HEAD
-import java.util.function.Supplier;
-
-/**
- * The type Entity not found exception.
- */
-public class EntityNotFoundException extends RuntimeException  {
-=======
 /**
  * The type Entity not found exception.
  */
 public class EntityNotFoundException extends RuntimeException {
->>>>>>> f6ab42249b6f6db1f62f964344dd357ee150b39c
   /**
    * Instantiates a new Entity not found exception.
    *
@@ -23,9 +14,4 @@ public class EntityNotFoundException extends RuntimeException {
     }
 
     private EntityNotFoundException(){}
-<<<<<<< HEAD
-
-
-=======
->>>>>>> f6ab42249b6f6db1f62f964344dd357ee150b39c
 }
